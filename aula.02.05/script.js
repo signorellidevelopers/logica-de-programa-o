a9dbabdfe4ade2509726b294ea243d106b909cf3
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2020-08-14 18:14:43 -0300

    atuaizado

---
+++ aula.02.05/script.js
@@ -0,0 +1,16 @@
+let n1 = window.prompt("digite um número");
+let n2 = window.prompt("digite outro número");
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+let s = n1 + n2;
+let media = s / 2;
+
+if( media >= 5){
+   alert("aprovado")
+}else{
+    
+  alert(  "reprovado")
+}
+
+
+
---
+++ base/script.js
@@ -0,0 +1,7 @@
+let n1 = window.prompt("digite um número");
+let n2 = window.prompt("digite outro número");
+n1 = parseFloat(n1);
+n2 = parseFloat(n2);
+let s = n1 + n2;
+let media = s / 2;
+window.alert("A média entre n1 e n2 é, " + media);
