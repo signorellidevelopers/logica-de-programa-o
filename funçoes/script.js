c9bd51a0c3a9411e88b1703d6068f0288de89f3d
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2020-08-18 21:03:09 -0300

    Atualizado

--- funçoes/script.js
+++ funçoes/script.js
@@ -1,3 +1,6 @@
-let output = document.querySelector('output');
-
-output.innerText  "texto inserido por js"
+let output = document.querySelector('#output');
+let msg = '';
+for( let i = 0; i<= 100; i++){
+    msg += 'i:' + i + '<br>';
+}
+output.innerHTML = msg
