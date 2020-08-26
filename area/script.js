1867a900d25b4333cc26728c477adb6e3d054012
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2020-08-26 15:15:56 -0300

    atualizado

--- area/script.js
+++ area/script.js
@@ -1,3 +1,8 @@
-let l1 = prompt('Digite o primeiro lad');
+let l1 = prompt('Digite o primeiro lado');
+ l1 = Number(l1);
 let l2 = prompt('Digite  o outro lado');
-let
+ l2 = Number(l2);
+
+function area(l1,l2){
+    alert('Área total é ' + l1 * l2  +  'M2');
+}
